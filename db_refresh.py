607123725ead5f9114f9e44b227a5702f7208061
ktwj <asahi_1282@yahoo.co.jp>
2021-11-22 17:29:49 +0900

    commit 11/22 6

--- db_refresh.py
+++ db_refresh.py
@@ -8,29 +8,28 @@ from pysrc.test import del_all_list
 from flask import session as ss
 import pandas as pd
 
-def exe():
-    database = 'dgp2oe0v7u7ba'
-    user = 'fcjytqjkmrdcth'
-    password = 'd0d4acde298c07fedd9acf037ec024ffd15a8ee6ea2ea07876d8ab7dc840b256'
-    host = 'ec2-23-23-181-251.compute-1.amazonaws.com'
-    port = '5432'
+database = 'dgp2oe0v7u7ba'
+user = 'fcjytqjkmrdcth'
+password = 'd0d4acde298c07fedd9acf037ec024ffd15a8ee6ea2ea07876d8ab7dc840b256'
+host = 'ec2-23-23-181-251.compute-1.amazonaws.com'
+port = '5432'
 
-    uri = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}?client_encoding=utf8'
+uri = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}?client_encoding=utf8'
 
-    engine = create_engine(uri,echo=True,encoding='utf-8')
+engine = create_engine(uri,echo=True,encoding='utf-8')
 
-    session = scoped_session(
-        sessionmaker(
-            autoflush = False,
-            autocommit = False,
-            bind = engine,
-        )
+session = scoped_session(
+    sessionmaker(
+        autoflush = False,
+        autocommit = False,
+        bind = engine,
     )
+)
 
-    db = declarative_base()
-    db.query = session.query_property()
-    del_all_list()
+db = declarative_base()
+db.query = session.query_property()
+del_all_list()
 
-    sl = getter()
-    sl.to_sql('search_list', con=engine, if_exists='append')
-    del sl
+sl = getter()
+sl.to_sql('search_list', con=engine, if_exists='append')
+del sl
