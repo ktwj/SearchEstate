f211687b6a7ee80dd343f50e07783cf6eae262db
ktwj <asahi_1282@yahoo.co.jp>
2021-12-13 10:27:38 +0900

    commit 11/22 6

---
+++ ap.js
@@ -0,0 +1,22 @@
+member = [
+    {
+      "id": 1,
+      "name": "tanaka tarou"
+    },
+    {
+      "id": 2,
+      "name": "satou jirou"
+    },
+    {
+      "id": 3,
+      "name": "suzuki hanako"
+    },
+    {
+      "id": 4,
+      "name": "matsuda ichirou"
+    }
+  ];
+
+member.forEach(function(mem){
+    console.log(mem.name);
+});
