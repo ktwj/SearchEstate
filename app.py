ce7cc7fcf1561b4b21c00d749ca012d088d6644d
ktwj <asahi_1282@yahoo.co.jp>
2021-12-13 11:23:19 +0900

    commit 12/

--- app.py
+++ app.py
@@ -161,7 +161,8 @@ def lists(user_id):
 def favlist(user_id):
     if session_time_check():return redirect('/')
 
-    rooms = list_of_fav(user_id).reverse()
+    rooms = list_of_fav(user_id)
+    rooms.reverse()
     return render_template('fav.html', rooms=rooms)
 
 """
